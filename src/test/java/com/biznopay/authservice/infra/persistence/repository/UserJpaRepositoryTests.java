27ba05e9dc5ab4cadb019fa65700611d8cc96374
Gervásio Artur <90800218+gervasioartur@users.noreply.github.com>
2026-05-09 01:15:03 +0200

    chore: improve code quality

--- src/test/java/com/biznopay/authservice/infra/persistence/repository/UserJpaRepositoryTests.java
+++ src/test/java/com/biznopay/authservice/infra/persistence/repository/UserJpaRepositoryTests.java
@@ -4,7 +4,6 @@ import com.biznopay.authservice.domain.entity.user.SuperAdmin;
 import com.biznopay.authservice.domain.entity.user.User;
 import com.biznopay.authservice.infra.mapper.UserMapper;
 import com.biznopay.authservice.infra.persistence.jpa.entity.UserJpaEntity;
-import com.biznopay.authservice.infra.persistence.jpa.repository.SuperAdminJpaRepository;
 import com.biznopay.authservice.infra.persistence.jpa.repository.UserJpaRepository;
 import com.biznopay.authservice.usecase.user.register.sa.RegisterSAInput;
 import org.junit.jupiter.api.Assertions;
@@ -52,7 +51,7 @@ public class UserJpaRepositoryTests {
 
     @Test
     @DisplayName("Should return user when exist on find by email")
-    public  void shouldReturnUserWhenExistOnFindByEmail() {
+    public void shouldReturnUserWhenExistOnFindByEmail() {
         RegisterSAInput input = new RegisterSAInput("any_first_name", "any_last_name", "admin@bizno.co.mz", "Password@123");
         User user = SuperAdmin.register(input.firstName(), input.lastName(), input.email(), input.password());
         UserJpaEntity entity = UserMapper.toUserJpaEntity(user);
