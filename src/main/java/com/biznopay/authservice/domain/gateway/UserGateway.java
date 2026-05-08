703b70204036fa52312df25fb34ee4c4af789b4f
Gervásio Artur <90800218+gervasioartur@users.noreply.github.com>
2026-05-08 22:40:53 +0200

    feat: ensure RegisterSA throws EmailAlreadyInUseException when email is already in use on register super admin

--- src/main/java/com/biznopay/authservice/domain/gateway/UserGateway.java
+++ src/main/java/com/biznopay/authservice/domain/gateway/UserGateway.java
@@ -1,6 +1,5 @@
 package com.biznopay.authservice.domain.gateway;
 
-import com.biznopay.authservice.domain.entity.user.SuperAdmin;
 import com.biznopay.authservice.domain.entity.user.User;
 
 import java.util.Optional;
@@ -8,4 +7,5 @@ import java.util.Optional;
 public interface UserGateway {
     long countSAs();
     void save(User user);
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/biznopay/authservice/usecase/user/register/sa/RegisterSA.java
+++ src/main/java/com/biznopay/authservice/usecase/user/register/sa/RegisterSA.java
@@ -1,6 +1,7 @@
 package com.biznopay.authservice.usecase.user.register.sa;
 
 import com.biznopay.authservice.domain.entity.user.SuperAdmin;
+import com.biznopay.authservice.domain.entity.user.User;
 import com.biznopay.authservice.domain.exception.ConflictException;
 import com.biznopay.authservice.domain.gateway.UserGateway;
 
@@ -16,6 +17,8 @@ public class RegisterSA {
     public RegisterSAOutput execute(RegisterSAInput input) {
         long contSAs = userGateway.countSAs();
         if (contSAs > 0) throw new ConflictException("Super admin", "SUPER_ADMIN-003");
+        Optional<User> user = userGateway.findByEmail(input.email());
+        if (user.isPresent()) throw new ConflictException("Email already in use", "SUPER_ADMIN-004");
         SuperAdmin superAdmin = SuperAdmin.register(input.firstName(), input.lastName(), input.email(), input.password());
         userGateway.save(superAdmin);
         return new RegisterSAOutput("We've sent an activation link to provided email: " + input.email());
