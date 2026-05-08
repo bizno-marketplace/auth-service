c34551b0212f6f858942835a8faf151bd387475a
Gervásio Artur <90800218+gervasioartur@users.noreply.github.com>
2026-05-09 01:14:18 +0200

    test: ensure UserJpaRepository user when exist on find by email

--- src/test/java/com/biznopay/authservice/infra/persistence/repository/UserJpaRepositoryTests.java
+++ src/test/java/com/biznopay/authservice/infra/persistence/repository/UserJpaRepositoryTests.java
@@ -1,8 +1,12 @@
 package com.biznopay.authservice.infra.persistence.repository;
 
+import com.biznopay.authservice.domain.entity.user.SuperAdmin;
+import com.biznopay.authservice.domain.entity.user.User;
+import com.biznopay.authservice.infra.mapper.UserMapper;
 import com.biznopay.authservice.infra.persistence.jpa.entity.UserJpaEntity;
 import com.biznopay.authservice.infra.persistence.jpa.repository.SuperAdminJpaRepository;
 import com.biznopay.authservice.infra.persistence.jpa.repository.UserJpaRepository;
+import com.biznopay.authservice.usecase.user.register.sa.RegisterSAInput;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -44,5 +48,28 @@ public class UserJpaRepositoryTests {
     public void shouldReturnOptionalEmptyWhenUserNotExistOnFindByEmail() {
         Optional<UserJpaEntity> user = userJpaRepository.findByEmail("any_email");
         Assertions.assertTrue(user.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return user when exist on find by email")
+    public  void shouldReturnUserWhenExistOnFindByEmail() {
+        RegisterSAInput input = new RegisterSAInput("any_first_name", "any_last_name", "admin@bizno.co.mz", "Password@123");
+        User user = SuperAdmin.register(input.firstName(), input.lastName(), input.email(), input.password());
+        UserJpaEntity entity = UserMapper.toUserJpaEntity(user);
+        userJpaRepository.save(entity);
+
+        Optional<UserJpaEntity> result = userJpaRepository.findByEmail("admin@bizno.co.mz");
+
+        Assertions.assertTrue(result.isPresent());
+        Assertions.assertEquals(entity.getId(), result.get().getId());
+        Assertions.assertEquals(entity.getFirstName(), result.get().getFirstName());
+        Assertions.assertEquals(entity.getLastName(), result.get().getLastName());
+        Assertions.assertEquals(entity.getEmail(), result.get().getEmail());
+        Assertions.assertEquals("", result.get().getPhone());
+        Assertions.assertEquals(entity.getPassword(), result.get().getPassword());
+        Assertions.assertEquals(entity.getStatus(), result.get().getStatus());
+        Assertions.assertEquals(entity.getExpiresAt(), result.get().getExpiresAt());
+        Assertions.assertEquals(entity.getCreatedAt(), result.get().getCreatedAt());
+        Assertions.assertEquals(entity.getUpdatedAt(), result.get().getUpdatedAt());
     }
 }
