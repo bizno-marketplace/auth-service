374a3ff70c83fd6df30ce1e8aab2a4c4ff8d02d1
Gervásio Artur <90800218+gervasioartur@users.noreply.github.com>
2026-05-08 13:05:20 +0200

    chore: adding project structure

---
+++ src/test/java/com/biznopay/authservice/TestAuthServiceApplication.java
@@ -0,0 +1,11 @@
+package com.biznopay.authservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(AuthServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
