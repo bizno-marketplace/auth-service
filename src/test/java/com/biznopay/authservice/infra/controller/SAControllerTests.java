3d68d5782dc6d7e7c746695f53b78c5866cea4bc
Gervásio Artur <90800218+gervasioartur@users.noreply.github.com>
2026-05-09 02:38:14 +0200

    test: ensure SAController returns 400 if last name is empty

--- src/test/java/com/biznopay/authservice/infra/controller/SAControllerTests.java
+++ src/test/java/com/biznopay/authservice/infra/controller/SAControllerTests.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
@@ -55,13 +57,14 @@ public class SAControllerTests {
         return "http://localhost:" + port + path;
     }
 
+    @EmptySource
     @ParameterizedTest
-    @NullAndEmptySource
-    @DisplayName("Should return 400 if first name is null or empty")
-    void shouldReturn400IfFirstNameIsNullOrEmpty(String firstName) {
+    @DisplayName("Should return 400 if first name is empty")
+    void shouldReturn400IfFirstNameIsEmpty(String firstName) {
         RegisterSARequest request = new RegisterSARequest(firstName, "Smith", "johnsmith@bizno.co.mz", "Password@123");
         ResponseEntity<ApiResponse> response = restTemplate.postForEntity(url("/supper-admins"), request, ApiResponse.class);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assertions.assertEquals("First name is required", response.getBody().error().message());
     }
 
     @Test
@@ -71,6 +74,16 @@ public class SAControllerTests {
         ResponseEntity<ApiResponse> response = restTemplate.postForEntity(url("/supper-admins"), request, ApiResponse.class);
         Assertions.assertEquals(HttpStatus.UNPROCESSABLE_CONTENT, response.getStatusCode());
         Assertions.assertEquals("First name must be at least 3 characters long", response.getBody().error().message());
+    }
+
+    @EmptySource
+    @ParameterizedTest
+    @DisplayName("Should return 400 if last name is empty")
+    void shouldReturn400IfLastNameIsEmpty(String lastname) {
+        RegisterSARequest request = new RegisterSARequest("John", lastname, "johnsmith@bizno.co.mz", "Password@123");
+        ResponseEntity<ApiResponse> response = restTemplate.postForEntity(url("/supper-admins"), request, ApiResponse.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assertions.assertEquals("Last name is required", response.getBody().error().message());
     }
 
 }
