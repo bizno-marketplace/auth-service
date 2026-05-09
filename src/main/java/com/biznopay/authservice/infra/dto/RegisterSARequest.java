6135f90b1ee2b36ef27d6502af903dd082eb1dc4
Gervásio Artur <90800218+gervasioartur@users.noreply.github.com>
2026-05-09 02:38:09 +0200

    feat: ensure SAController returns 400 if last name is empty

--- src/main/java/com/biznopay/authservice/infra/dto/RegisterSARequest.java
+++ src/main/java/com/biznopay/authservice/infra/dto/RegisterSARequest.java
@@ -1,24 +1,18 @@
 package com.biznopay.authservice.infra.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 public record RegisterSARequest(
         @NotEmpty(message = "First name is required")
-        @NotNull(message = "First name is required")
         String firstName,
 
         @NotEmpty(message = "Last name is required")
-        @NotNull(message = "Last name is required")
         String lastName,
 
         @NotEmpty(message = "E-mail is required")
-        @NotNull(message = "E-mail is required")
         String email,
 
-
         @NotEmpty(message = "Password is required")
-        @NotNull(message = "Password is required")
         String password
 ) {
 }
