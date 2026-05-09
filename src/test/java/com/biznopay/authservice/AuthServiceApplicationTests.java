83c19f5552b0475edd79429fe7e1ba2ce214e0d0
Gervásio Artur <90800218+gervasioartur@users.noreply.github.com>
2026-05-09 18:51:40 +0200

    chore: exclude AuthServiceApplication on coverage

--- src/test/java/com/biznopay/authservice/AuthServiceApplicationTests.java
+++ src/test/java/com/biznopay/authservice/AuthServiceApplicationTests.java
@@ -1,9 +1,14 @@
 package com.biznopay.authservice;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -13,5 +18,4 @@ class AuthServiceApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
