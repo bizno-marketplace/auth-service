29ce6ea164129f46beb0ce9369ee54f3c40ecea8
Gervásio Artur <90800218+gervasioartur@users.noreply.github.com>
2026-05-10 19:42:52 +0200

    fix: redo removing unuseful tests

---
+++ src/test/java/com/biznopay/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.biznopay.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
