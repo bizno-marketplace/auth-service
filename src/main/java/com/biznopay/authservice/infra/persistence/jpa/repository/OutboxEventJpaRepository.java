5636808cded09124b97281ea0ba1d88200140413
gervasiodombo <gervasiorthur@gmil.com>
2026-05-11 20:53:43 +0200

    feat: ensure OutboxEventJpaRepository returns correct number of Outbox

---
+++ src/main/java/com/biznopay/authservice/infra/persistence/jpa/repository/OutboxEventJpaRepository.java
@@ -0,0 +1,12 @@
+package com.biznopay.authservice.infra.persistence.jpa.repository;
+
+import com.biznopay.authservice.infra.outbox.OutboxStatus;
+import com.biznopay.authservice.infra.persistence.jpa.entity.OutboxEventJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OutboxEventJpaRepository extends JpaRepository<OutboxEventJpaEntity, UUID> {
+    List<OutboxEventJpaEntity> findByStatus(OutboxStatus status);
+}
