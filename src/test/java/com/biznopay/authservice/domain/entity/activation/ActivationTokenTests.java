c4efc54894573fbef6e5e9cabdce47fb02f744b0
gervasiodombo <gervasiorthur@gmil.com>
2026-05-11 11:49:57 +0200

    test: ensure ActivationToken throws RequiredFieldException user id is null on generate

--- src/test/java/com/biznopay/authservice/domain/entity/activation/ActivationTokenTests.java
+++ src/test/java/com/biznopay/authservice/domain/entity/activation/ActivationTokenTests.java
@@ -1,10 +1,14 @@
 package com.biznopay.authservice.domain.entity.activation;
 
+import com.biznopay.authservice.domain.entity.user.UserId;
 import com.biznopay.authservice.domain.exception.InvalidEntityIdException;
 import com.biznopay.authservice.domain.exception.RequiredFieldException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 public class ActivationTokenTests {
     @Test
@@ -17,5 +21,22 @@ public class ActivationTokenTests {
     @DisplayName("Should throw RequiredFieldException if user id is null on generate")
     public void shouldThrowRequiredFieldExceptionIfUserIdIsNullOnGenerate() {
         Assertions.assertThrows(RequiredFieldException.class, () -> ActivationToken.generate(null));
+    }
+
+    @Test
+    @DisplayName("Should generate ActivationToken with correct values ")
+    public void shouldGenerateActivationTokenWithCorrectValues(){
+        UserId userId = new UserId(UUID.randomUUID());
+        ActivationToken activationToken = ActivationToken.generate(userId);
+
+        Assertions.assertNotNull(activationToken.getId());
+        Assertions.assertEquals(userId, activationToken.getUserId());
+        long minutes = ChronoUnit.MINUTES.between(activationToken.getCreatedAt(), activationToken.getExpiresAt());
+        Assertions.assertEquals(ActivationToken.EXPIRATION_MINUTES, minutes);
+        Assertions.assertFalse(activationToken.isExpired());
+        Assertions.assertFalse(activationToken.isUsed());
+        Assertions.assertTrue(activationToken.isValid());
+        Assertions.assertNotNull(activationToken.getCreatedAt());
+        Assertions.assertNotNull(activationToken.getExpiresAt());
     }
 }
