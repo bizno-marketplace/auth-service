30b71250cb919f6a1ff7cc11fb81722eb1338820
gervasiodombo <gervasiorthur@gmil.com>
2026-05-11 13:27:51 +0200

    chore: improve code quality

--- src/main/java/com/biznopay/authservice/infra/mapper/ActivationTokenMapper.java
+++ src/main/java/com/biznopay/authservice/infra/mapper/ActivationTokenMapper.java
@@ -4,7 +4,7 @@ import com.biznopay.authservice.domain.entity.activation.ActivationToken;
 import com.biznopay.authservice.infra.persistence.jpa.entity.ActivationTokenJpaEntity;
 
 public class ActivationTokenMapper {
-    public static  ActivationTokenJpaEntity toJpaEntity(ActivationToken token) {
+    public static ActivationTokenJpaEntity toJpaEntity(ActivationToken token) {
         return new ActivationTokenJpaEntity(token.getId().value(), token.getUserId().value(), token.isUsed(),
                 token.getExpiresAt(), token.getCreatedAt());
     }
