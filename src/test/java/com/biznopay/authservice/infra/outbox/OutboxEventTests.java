05c50d09d35edef464edddcabebf0ba62a116992
gervasiodombo <gervasiorthur@gmil.com>
2026-05-11 19:25:49 +0200

    test: ensure OutboxEvent throws RequiredFieldException if eventType is null or empty

--- src/test/java/com/biznopay/authservice/infra/outbox/OutboxEventTests.java
+++ src/test/java/com/biznopay/authservice/infra/outbox/OutboxEventTests.java
@@ -5,11 +5,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 public class OutboxEventTests {
     @Test
     @DisplayName("Should throw RequiredFieldException if aggregateId is null on create")
     public void shouldThrowRequiredFieldExceptionIfAggregateIdIsNullOnCreate() {
-        Assertions.assertThrows(RequiredFieldException
-                .class, () -> OutboxEvent.create(null, "eventType", "subject", "payload"));
+        Assertions.assertThrows(RequiredFieldException.class, () ->
+                OutboxEvent.create(null, "eventType", "subject", "payload"));
+    }
+
+    @Test
+    @DisplayName("Should RequiredFieldException if eventType is null or empty")
+    public void shouldThrowRequiredFieldExceptionIfEventTypeIsNullOrEmptyOnCreate() {
+        Assertions.assertThrows(RequiredFieldException.class, () ->
+                OutboxEvent.create(UUID.randomUUID(), null, "subject", "payload"));
     }
 }
