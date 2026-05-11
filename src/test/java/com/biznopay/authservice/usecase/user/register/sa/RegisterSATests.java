206d5084447b53e95c546df024c3164ccfe7a1d3
gervasiodombo <gervasiorthur@gmil.com>
2026-05-11 13:03:28 +0200

    test: ensure RegisterSA calls activationTokenGateway and domainEventGateway

--- src/test/java/com/biznopay/authservice/usecase/user/register/sa/RegisterSATests.java
+++ src/test/java/com/biznopay/authservice/usecase/user/register/sa/RegisterSATests.java
@@ -1,8 +1,12 @@
 package com.biznopay.authservice.usecase.user.register.sa;
 
+import com.biznopay.authservice.domain.entity.activation.ActivationToken;
+import com.biznopay.authservice.domain.entity.event.UserRegistered;
 import com.biznopay.authservice.domain.entity.user.SuperAdmin;
 import com.biznopay.authservice.domain.exception.ConflictException;
 import com.biznopay.authservice.domain.exception.EmailAlreadyInUseException;
+import com.biznopay.authservice.domain.gateway.ActivationTokenGateway;
+import com.biznopay.authservice.domain.gateway.DomainEventGateway;
 import com.biznopay.authservice.domain.gateway.UserGateway;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -19,12 +23,22 @@ public class RegisterSATests {
     @Mock
     private UserGateway userGateway;
 
+    @Mock
+    private ActivationTokenGateway activationTokenGateway;
+
+    @Mock
+    private DomainEventGateway domainEventGateway;
+
+    private RegisterSA setupRegisterSA() {
+        return new RegisterSA(userGateway, activationTokenGateway, domainEventGateway);
+    }
+
     @Test
     @DisplayName("Should throw ConflictException when system already has a super admin on register super admin")
     public void shouldThrowConflictExceptionWhenSystemHAlreadyHasASuperAdminOnRegisterSuperAdmin() {
         RegisterSAInput input = new RegisterSAInput("any_first_name", "any_last_name", "admin@bizno.co.mz", "Password@123");
         Mockito.when(userGateway.countSAs()).thenReturn(1L);
-        RegisterSA useCase = new RegisterSA(userGateway);
+        RegisterSA useCase = setupRegisterSA();
         Assertions.assertThrows(ConflictException.class, () -> useCase.execute(input), "Super admin already exists");
         Mockito.verify(userGateway, Mockito.times(1)).countSAs();
     }
@@ -36,7 +50,7 @@ public class RegisterSATests {
         SuperAdmin superAdmin = SuperAdmin.register(input.firstName(), input.lastName(), input.email(), input.password());
         Mockito.when(userGateway.countSAs()).thenReturn(0L);
         Mockito.when(userGateway.findByEmail(input.email())).thenReturn(Optional.of(superAdmin));
-        RegisterSA useCase = new RegisterSA(userGateway);
+        RegisterSA useCase = setupRegisterSA();
         Assertions.assertThrows(EmailAlreadyInUseException.class, () -> useCase.execute(input), "Email already in use");
         Mockito.verify(userGateway, Mockito.times(1)).countSAs();
         Mockito.verify(userGateway, Mockito.times(1)).findByEmail(input.email());
@@ -47,10 +61,14 @@ public class RegisterSATests {
     public void shouldRegisterSupperAdminSendActivationLinkWith15minsOfExpirationToProvidedEmailAndReturnMessageToNotifyUser() {
         RegisterSAInput input = new RegisterSAInput("any_first_name", "any_last_name", "admin@bizno.co.mz", "Password@123");
         Mockito.when(userGateway.countSAs()).thenReturn(0L);
-        RegisterSA useCase = new RegisterSA(userGateway);
+        Mockito.doNothing().when(activationTokenGateway).save(Mockito.any(ActivationToken.class));
+        Mockito.doNothing().when(domainEventGateway).publish(Mockito.any(UserRegistered.class));
+        RegisterSA useCase = setupRegisterSA();
         RegisterSAOutput output = useCase.execute(input);
         Assertions.assertEquals("We've sent an activation link to provided email: " + input.email(), output.message());
         Mockito.verify(userGateway, Mockito.times(1)).countSAs();
         Mockito.verify(userGateway, Mockito.times(1)).save(Mockito.any(SuperAdmin.class));
+        Mockito.verify(activationTokenGateway, Mockito.times(1)).save(Mockito.any(ActivationToken.class));
+        Mockito.verify(domainEventGateway, Mockito.times(1)).publish(Mockito.any(UserRegistered.class));
     }
 }
