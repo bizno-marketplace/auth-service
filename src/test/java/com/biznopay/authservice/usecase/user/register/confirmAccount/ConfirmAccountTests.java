9425164b7d4cb1ce578e678a7a0c7c9c917dd459
Gervásio Artur <gervasioarthur@gmail.com>
2026-05-12 21:05:24 +0200

    test: ensure ConfirmAccount throws ExpiredConfirmationTokenException if token has exprired

--- src/test/java/com/biznopay/authservice/usecase/user/register/confirmAccount/ConfirmAccountTests.java
+++ src/test/java/com/biznopay/authservice/usecase/user/register/confirmAccount/ConfirmAccountTests.java
@@ -1,5 +1,9 @@
 package com.biznopay.authservice.usecase.user.register.confirmAccount;
 
+import com.biznopay.authservice.domain.entity.activation.ActivationToken;
+import com.biznopay.authservice.domain.entity.activation.ActivationTokenId;
+import com.biznopay.authservice.domain.entity.user.UserId;
+import com.biznopay.authservice.domain.exception.ExpiredConfirmationTokenException;
 import com.biznopay.authservice.domain.exception.InvalidConfirmationTokenException;
 import com.biznopay.authservice.domain.gateway.ActivationTokenGateway;
 import com.biznopay.authservice.domain.gateway.UserGateway;
@@ -9,8 +13,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,4 +36,17 @@ public class ConfirmAccountTests {
         Assertions.assertThrows(InvalidConfirmationTokenException.class, () -> confirmAccount.execute(rawTokenId));
     }
 
+    @Test
+    @DisplayName("Should throw ExpiredConfirmationTokenException if token has expired")
+    public void shouldThrowExpiredConfirmationTokenExceptionIfTokenHasExpired() {
+        UUID rawTokenId = UUID.randomUUID();
+        UserId userId = new UserId(UUID.randomUUID());
+        ActivationTokenId activationTokenId = new ActivationTokenId(rawTokenId);
+        LocalDateTime expiredAt = LocalDateTime.now().minusMinutes(15);
+        ActivationToken activationToken = ActivationToken.reconstitute(activationTokenId, userId, false, expiredAt, expiredAt);
+        Mockito.when(tokenGateway.findById(rawTokenId)).thenReturn(Optional.of(activationToken));
+        ConfirmAccount confirmAccount = new ConfirmAccount(tokenGateway, userGateway);
+        Assertions.assertThrows(ExpiredConfirmationTokenException.class, () -> confirmAccount.execute(rawTokenId));
+        Assertions.assertFalse(activationToken.isValid());
+    }
 }
