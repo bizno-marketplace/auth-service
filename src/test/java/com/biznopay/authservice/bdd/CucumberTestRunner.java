43c2f711f282e90d752b3f0612df18d727711d68
gervasiodombo <gervasiorthur@gmil.com>
2026-05-12 12:07:36 +0200

    chore: configuring cucumber

---
+++ src/test/java/com/biznopay/authservice/bdd/CucumberTestRunner.java
@@ -0,0 +1,15 @@
+package com.biznopay.authservice.bdd;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.biznopay.authservice.bdd")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports/report.html")
+public class CucumberTestRunner {
+}
