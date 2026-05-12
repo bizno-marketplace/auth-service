d7b3b70840c85ca5219108bbfab19fb9c25bc08f
Gervásio Artur <gervasioarthur@gmail.com>
2026-05-12 20:06:56 +0200

    feat: ensure ConfirmAccount throws ResourceNotFoundException when token is not found

---
+++ src/main/java/com/biznopay/authservice/usecase/user/confirmAccount/ConfirmAccount.java
@@ -0,0 +1,21 @@
+package com.biznopay.authservice.usecase.user.confirmAccount;
+
+import com.biznopay.authservice.domain.exception.ResourceNotFoundException;
+import com.biznopay.authservice.domain.gateway.ActivationTokenGateway;
+import com.biznopay.authservice.domain.gateway.UserGateway;
+
+import java.util.UUID;
+
+public class ConfirmAccount {
+    private final ActivationTokenGateway tokenGateway;
+    private final UserGateway userGateway;
+
+    public ConfirmAccount(ActivationTokenGateway tokenGateway, UserGateway userGateway) {
+        this.tokenGateway = tokenGateway;
+        this.userGateway = userGateway;
+    }
+
+    public void execute(UUID rawTokenId) {
+        throw  new ResourceNotFoundException("Activation token", "ACTIVATION_TOKEN-001");
+    }
+}
