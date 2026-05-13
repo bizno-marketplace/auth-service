4746e772fc00cc1a9fcc5497ba52048894d9d4a7
gervasiodombo <gervasiorthur@gmil.com>
2026-05-13 10:40:12 +0200

    test: ensure AccountController returns 204 on successful account confirmation

---
+++ src/test/java/com/biznopay/authservice/infra/controller/AccountControllerTests.java
@@ -0,0 +1,71 @@
+package com.biznopay.authservice.infra.controller;
+
+import com.biznopay.authservice.config.PostgresContainerBase;
+import com.biznopay.authservice.config.TestConfig;
+import com.biznopay.authservice.domain.vo.ApiResponse;
+import com.biznopay.authservice.infra.persistence.jpa.entity.ActivationTokenJpaEntity;
+import com.biznopay.authservice.infra.persistence.jpa.entity.UserJpaEntity;
+import com.biznopay.authservice.infra.persistence.jpa.repository.ActivationTokenJpaRepository;
+import com.biznopay.authservice.infra.persistence.jpa.repository.UserJpaRepository;
+import com.biznopay.authservice.mocks.Mocks;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Tag("integration")
+@ActiveProfiles("test")
+@Import({TestConfig.class})
+@AutoConfigureRestTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AccountControllerTests extends PostgresContainerBase {
+    @LocalServerPort
+    private int port;
+
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private ActivationTokenJpaRepository activationTokenJpaRepository;
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+    private String url(String path) {
+        return "http://localhost:" + port + path;
+    }
+
+
+    @AfterAll
+    static void tearDown() {
+        if (postgres != null && postgres.isRunning()) {
+            postgres.stop();
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        restTemplate = new TestRestTemplate();
+        jdbcTemplate.execute("TRUNCATE TABLE t_activation_tokens RESTART IDENTITY CASCADE");
+    }
+
+    @Test
+    @DisplayName("Should return 204 on successful account confirmation")
+    void shouldReturn204OnSuccessfulAccountConfirmation() {
+        UserJpaEntity user = Mocks.buyerJpaEntityMock();
+        userJpaRepository.save(user);
+        ActivationTokenJpaEntity entity = Mocks.unusedActivationTokenJpaEntityFromBuyerMock(user);
+        activationTokenJpaRepository.save(entity);
+        ResponseEntity response = restTemplate.getForEntity(url("/accounts?token=" + entity.getId()), Void.class);
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+    }
+}
