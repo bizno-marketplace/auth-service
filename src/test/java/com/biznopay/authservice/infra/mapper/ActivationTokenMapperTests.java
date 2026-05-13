9c310cc5443fbb6097ff662a3b6b3d7a6bba1449
gervasiodombo <gervasiorthur@gmil.com>
2026-05-13 09:29:06 +0200

    test: ensure ActivationTokenMapper returns ActivationToken on toDomainEntity

--- src/test/java/com/biznopay/authservice/infra/mapper/ActivationTokenMapperTests.java
+++ src/test/java/com/biznopay/authservice/infra/mapper/ActivationTokenMapperTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class ActivationTokenMapperTests {
@@ -17,6 +18,19 @@ public class ActivationTokenMapperTests {
         UserId userId = new UserId(UUID.randomUUID());
         ActivationToken activationToken = ActivationToken.generate(userId);
         ActivationTokenJpaEntity entity = ActivationTokenMapper.toJpaEntity(activationToken);
+        Assertions.assertEquals(entity.getId(), activationToken.getId().value());
+        Assertions.assertEquals(entity.getUserId(), activationToken.getUserId().value());
+        Assertions.assertEquals(entity.isUsed(), activationToken.isUsed());
+        Assertions.assertEquals(entity.getExpiresAt(), activationToken.getExpiresAt());
+        Assertions.assertEquals(entity.getCreatedAt(), activationToken.getCreatedAt());
+    }
+
+    @Test
+    @DisplayName("Should return ActivationToken on toDomainEntity")
+    public void shouldReturnActivationTokenOntoDomainEntity(){
+        ActivationTokenJpaEntity entity = new ActivationTokenJpaEntity(UUID.randomUUID(),UUID.randomUUID(),false, LocalDateTime.now(),LocalDateTime.now());
+        ActivationToken activationToken =  ActivationTokenMapper.toDomainEntity(entity);
+
         Assertions.assertEquals(entity.getId(), activationToken.getId().value());
         Assertions.assertEquals(entity.getUserId(), activationToken.getUserId().value());
         Assertions.assertEquals(entity.isUsed(), activationToken.isUsed());
