abc006f1f07bde3abacdf998480a858ec351202d
gervasiodombo <gervasiorthur@gmil.com>
2026-05-13 08:54:16 +0200

    test: ensure ActivationTokenMapper return ActivationTokenJpaEntity en toJpaEntity

---
+++ src/test/java/com/biznopay/authservice/infra/mapper/ActivationTokenMapperTests.java
@@ -0,0 +1,25 @@
+package com.biznopay.authservice.infra.mapper;
+
+import com.biznopay.authservice.domain.entity.activation.ActivationToken;
+import com.biznopay.authservice.domain.entity.user.UserId;
+import com.biznopay.authservice.infra.persistence.jpa.entity.ActivationTokenJpaEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class ActivationTokenMapperTests {
+    @Test
+    @DisplayName("Should return ActivationTokenJpaEntity on toJpaEntity")
+    public void shouldReturnActivationTokenJpaEntityOnToJpaEntity() {
+        UserId userId =  new UserId(UUID.randomUUID());
+        ActivationToken  activationToken =  ActivationToken.generate(userId);
+        ActivationTokenJpaEntity entity =  ActivationTokenMapper.toJpaEntity(activationToken);
+        Assertions.assertEquals(entity.getId(), activationToken.getId().value());
+        Assertions.assertEquals(entity.getUserId(), activationToken.getUserId().value());
+        Assertions.assertEquals(entity.isUsed(), activationToken.isUsed());
+        Assertions.assertEquals(entity.getExpiresAt(), activationToken.getExpiresAt());
+        Assertions.assertEquals(entity.getCreatedAt(), activationToken.getCreatedAt());
+    }
+}
