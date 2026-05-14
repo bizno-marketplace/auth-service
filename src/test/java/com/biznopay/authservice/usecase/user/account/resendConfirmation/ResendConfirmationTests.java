6a73276f485a1c044293859852bfca999b728232
gervasiodombo <gervasiorthur@gmil.com>
2026-05-14 10:39:48 +0200

    test: ensure ResendConformation throws AccountAlreadyConfirmedException when account has confirmed

---
+++ src/test/java/com/biznopay/authservice/usecase/user/account/resendConfirmation/ResendConfirmationTests.java
@@ -0,0 +1,32 @@
+package com.biznopay.authservice.usecase.user.account.resendConfirmation;
+
+import com.biznopay.authservice.domain.entity.user.User;
+import com.biznopay.authservice.domain.exception.AccountAlreadyConfirmedException;
+import com.biznopay.authservice.domain.gateway.UserGateway;
+import com.biznopay.authservice.mocks.Mocks;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+public class ResendConfirmationTests {
+    @Mock
+    private UserGateway userGateway;
+
+    @Test
+    @DisplayName("Should throw AccountAlreadyConfirmedException when account has confirmed ")
+    public void shouldThrowAccountAlreadyConfirmedExceptionWhenAccountHasConfirmed() {
+        String email = "user@example.com";
+        User user = Mocks.buyerMock();
+        user.activate();
+        Mockito.when(userGateway.findByEmail(email)).thenReturn(Optional.of(user));
+        ResendConformation resendConformation =  new ResendConformation(userGateway);
+       Assertions.assertThrows(AccountAlreadyConfirmedException.class, () -> resendConformation.execute(email));
+    }
+}
