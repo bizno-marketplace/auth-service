b9c5a6d899e96a252b384460be32dc67eb2c8cc1
gervasiodombo <gervasiorthur@gmil.com>
2026-05-14 10:51:54 +0200

    feat: ensure ResendConformation returns successfully message when account does not exist

--- src/main/java/com/biznopay/authservice/usecase/user/account/resendConfirmation/ResendConformation.java
+++ src/main/java/com/biznopay/authservice/usecase/user/account/resendConfirmation/ResendConformation.java
@@ -1,7 +1,11 @@
 package com.biznopay.authservice.usecase.user.account.resendConfirmation;
 
+import com.biznopay.authservice.domain.entity.user.User;
+import com.biznopay.authservice.domain.enums.UserStatus;
 import com.biznopay.authservice.domain.exception.AccountAlreadyConfirmedException;
 import com.biznopay.authservice.domain.gateway.UserGateway;
+
+import java.util.Optional;
 
 public class ResendConformation {
     private final UserGateway userGateway;
@@ -10,8 +14,11 @@ public class ResendConformation {
         this.userGateway = userGateway;
     }
 
-    public  void execute(String email) {
-        userGateway.findByEmail(email);
-        throw new AccountAlreadyConfirmedException("RESNED_CONFIRMATION-001");
+    public  String execute(String email) {
+        Optional<User> existingUser = userGateway.findByEmail(email);
+        if (existingUser.isEmpty()) return "Successfully requested a new confirmation email.";
+        User user = existingUser.get();
+        if (user.getStatus() == UserStatus.ACTIVE) throw new AccountAlreadyConfirmedException("RESNED_CONFIRMATION-001");
+        return null;
     }
 }
