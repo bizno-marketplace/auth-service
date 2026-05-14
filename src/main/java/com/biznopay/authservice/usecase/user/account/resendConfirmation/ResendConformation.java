f53b81092201c34aad3fdab69458c550d511530e
gervasiodombo <gervasiorthur@gmil.com>
2026-05-14 10:39:37 +0200

    feat: ensure ResendConformation throws AccountAlreadyConfirmedException when account has confirmed

--- src/main/java/com/biznopay/authservice/usecase/user/account/resendConfirmation/ResendConformation.java
+++ src/main/java/com/biznopay/authservice/usecase/user/account/resendConfirmation/ResendConformation.java
@@ -1,4 +1,17 @@
 package com.biznopay.authservice.usecase.user.account.resendConfirmation;
 
+import com.biznopay.authservice.domain.exception.AccountAlreadyConfirmedException;
+import com.biznopay.authservice.domain.gateway.UserGateway;
+
 public class ResendConformation {
+    private final UserGateway userGateway;
+
+    public ResendConformation(UserGateway userGateway) {
+        this.userGateway = userGateway;
+    }
+
+    public  void execute(String email) {
+        userGateway.findByEmail(email);
+        throw new AccountAlreadyConfirmedException("RESNED_CONFIRMATION-001");
+    }
 }
