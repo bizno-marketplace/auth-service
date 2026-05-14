d0b68c1fcdfe936535246e4322c06eaf108b3610
gervasiodombo <gervasiorthur@gmil.com>
2026-05-14 11:40:41 +0200

    test: ensure ResendConformation returns successfully message when account does not exist

--- src/test/java/com/biznopay/authservice/usecase/user/account/resendConfirmation/ResendConfirmationTests.java
+++ src/test/java/com/biznopay/authservice/usecase/user/account/resendConfirmation/ResendConfirmationTests.java
@@ -2,6 +2,10 @@ package com.biznopay.authservice.usecase.user.account.resendConfirmation;
 
 import com.biznopay.authservice.domain.entity.user.User;
 import com.biznopay.authservice.domain.exception.AccountAlreadyConfirmedException;
+import com.biznopay.authservice.domain.exception.TokenCooldownException;
+import com.biznopay.authservice.domain.gateway.ActivationTokenGateway;
+import com.biznopay.authservice.domain.gateway.DomainEventGateway;
+import com.biznopay.authservice.domain.gateway.ResendCooldownGateway;
 import com.biznopay.authservice.domain.gateway.UserGateway;
 import com.biznopay.authservice.mocks.Mocks;
 import org.junit.jupiter.api.Assertions;
@@ -18,13 +22,23 @@ import java.util.Optional;
 public class ResendConfirmationTests {
     @Mock
     private UserGateway userGateway;
+    @Mock
+    private  DomainEventGateway domainEventGateway;
+    @Mock
+    private ResendCooldownGateway resendCooldownGateway;
+    @Mock
+    private ActivationTokenGateway activationTokenGateway;
+
+    private ResendConformation setUp(){
+        return new ResendConformation(userGateway,domainEventGateway,resendCooldownGateway,activationTokenGateway);
+    }
 
     @Test
     @DisplayName("Should return successfully message when account does not exist")
     public void shouldReturnSuccessfullyMessageWhenAccountDoesNotExist(){
         String email = "user@example.com";
         Mockito.when(userGateway.findByEmail(email)).thenReturn(Optional.empty());
-        ResendConformation resendConformation =  new ResendConformation(userGateway);
+        ResendConformation resendConformation =  setUp();
         String result = resendConformation.execute(email);
         Assertions.assertEquals("Successfully requested a new confirmation email.", result);
     }
@@ -36,7 +50,18 @@ public class ResendConfirmationTests {
         User user = Mocks.buyerMock();
         user.activate();
         Mockito.when(userGateway.findByEmail(email)).thenReturn(Optional.of(user));
-        ResendConformation resendConformation =  new ResendConformation(userGateway);
+        ResendConformation resendConformation =  setUp();
         Assertions.assertThrows(AccountAlreadyConfirmedException.class, () -> resendConformation.execute(email));
+    }
+
+    @Test
+    @DisplayName("Should throw TokenCooldownException when account is on cooldown")
+    public void shouldThrowTokenCooldownExceptionWhenAccountIsOnCoolDown(){
+        String email = "user@example.com";
+        User user = Mocks.buyerMock();
+        Mockito.when(userGateway.findByEmail(email)).thenReturn(Optional.of(user));
+        Mockito.when(resendCooldownGateway.isInCooldown(email)).thenReturn(true);
+        ResendConformation resendConformation =  setUp();
+        Assertions.assertThrows(TokenCooldownException.class, () -> resendConformation.execute(email));
     }
 }
