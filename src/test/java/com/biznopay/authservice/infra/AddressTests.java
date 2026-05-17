91d660efa409121d0bf6396e8d7d8d52ee890770
Gervásio Artur <gervasioarthur@gmail.com>
2026-05-17 21:55:58 +0200

    test: ensure Address builds address with correct values

--- src/test/java/com/biznopay/authservice/infra/AddressTests.java
+++ src/test/java/com/biznopay/authservice/infra/AddressTests.java
@@ -35,4 +35,17 @@ public class AddressTests {
         Assertions.assertThrows(InvalidFieldException.class, () ->
                 new Address(32.5732, -999.00, null, null, null, null, null));
     }
+
+    @Test
+    @DisplayName("Should build Address with correct values on build address")
+    public void shouldBuildAddressWithCorrectValuesOnBuildAddress(){
+        Address address = new Address(32.5732, 32.5732, "any_street", "any_neighbourhood", "any_city", "any_province", "any_country");
+        Assertions.assertEquals(32.5732, address.latitude());
+        Assertions.assertEquals(32.5732, address.longitude());
+        Assertions.assertEquals("any_street", address.street());
+        Assertions.assertEquals("any_neighbourhood", address.neighbourhood());
+        Assertions.assertEquals("any_city", address.city());
+        Assertions.assertEquals("any_province", address.province());
+        Assertions.assertEquals("any_country", address.country());
+    }
 }
