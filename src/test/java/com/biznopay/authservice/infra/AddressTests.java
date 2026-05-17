37c9143c7c9c54b11f43d6ae79f697d313e482be
Gervásio Artur <gervasioarthur@gmail.com>
2026-05-17 21:50:40 +0200

    test: ensure Address throws RequiredFieldException when longitude is null on build

--- src/test/java/com/biznopay/authservice/infra/AddressTests.java
+++ src/test/java/com/biznopay/authservice/infra/AddressTests.java
@@ -29,4 +29,11 @@ public class AddressTests {
         Assertions.assertThrows(InvalidFieldException.class, () ->
                 new Address(-999.00, 32.5732, null, null, null, null, null));
     }
+
+    @Test
+    @DisplayName("Should throw InvalidFieldException when longitude is out of bounds on build address")
+    public void shouldThrowInvalidFieldWhenLongitudeIsOutOfBoundsExceptionOnBuildAddress() {
+        Assertions.assertThrows(InvalidFieldException.class, () ->
+                new Address(32.5732, -999.00, null, null, null, null, null));
+    }
 }
