f4834547fd9296411f3c742ff26384430c79695e
Gervásio Artur <gervasioarthur@gmail.com>
2026-05-17 21:44:08 +0200

    test: ensure Address throws RequiredFieldException when latitude is null on build

--- src/test/java/com/biznopay/authservice/infra/AddressTests.java
+++ src/test/java/com/biznopay/authservice/infra/AddressTests.java
@@ -1,6 +1,7 @@
 package com.biznopay.authservice.infra;
 
 import com.biznopay.authservice.domain.exception.InvalidFieldException;
+import com.biznopay.authservice.domain.exception.RequiredFieldException;
 import com.biznopay.authservice.domain.vo.Address;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
@@ -9,8 +10,15 @@ import org.junit.jupiter.api.Test;
 
 public class AddressTests {
     @Test
+    @DisplayName("Should throw RequiredFieldException when latitude is null on build")
+    public void shouldThrowRequiredFieldExceptionOnBuildAddress() {
+        Assertions.assertThrows(RequiredFieldException.class, () ->
+                new Address(null, 32.5732, null, null, null, null, null));
+    }
+
+    @Test
     @DisplayName("Should throw InvalidFieldException when latitude is out of bounds on build Address")
-    public void shouldThrowInvalidFieldExceptionOnBuildAddress() {
+    public void shouldThrowInvalidFieldWhenLatitudeIsOutOfBoundsExceptionOnBuildAddress() {
         Assertions.assertThrows(InvalidFieldException.class, () ->
                 new Address(-999.00, 32.5732, null, null, null, null, null));
     }
