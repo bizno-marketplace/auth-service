c3d8a02d3ff160dce738ed1e18eaa0a86f5bfd82
gervasiodombo <gervasiorthur@gmil.com>
2026-05-18 09:38:38 +0200

    feat: ensure RegisterBuyer registry buyer, send activation link with 15 min of expiration to provided email and return message to notify user

--- src/main/java/com/biznopay/authservice/usecase/user/register/buyer/RegisterBuyer.java
+++ src/main/java/com/biznopay/authservice/usecase/user/register/buyer/RegisterBuyer.java
@@ -1,7 +1,13 @@
 package com.biznopay.authservice.usecase.user.register.buyer;
 
+import com.biznopay.authservice.domain.entity.activation.ActivationToken;
+import com.biznopay.authservice.domain.entity.event.UserRegistered;
+import com.biznopay.authservice.domain.entity.user.Buyer;
 import com.biznopay.authservice.domain.entity.user.User;
 import com.biznopay.authservice.domain.exception.EmailAlreadyInUseException;
+import com.biznopay.authservice.domain.gateway.ActivationTokenGateway;
+import com.biznopay.authservice.domain.gateway.DomainEventGateway;
+import com.biznopay.authservice.domain.gateway.EncoderGateway;
 import com.biznopay.authservice.domain.gateway.UserGateway;
 
 import java.util.Optional;
@@ -10,15 +16,29 @@ import static com.biznopay.authservice.domain.util.DomainFuncUtils.validatePassw
 
 public class RegisterBuyer {
     private final UserGateway userGateway;
+    private final EncoderGateway encoderGateway;
+    private final DomainEventGateway domainEventGateway;
+    private final ActivationTokenGateway activationTokenGateway;
 
-    public RegisterBuyer(UserGateway userGateway) {
+    public RegisterBuyer(UserGateway userGateway, EncoderGateway encoderGateway, DomainEventGateway domainEventGateway, 
+                         ActivationTokenGateway activationTokenGateway) {
         this.userGateway = userGateway;
+        this.encoderGateway = encoderGateway;
+        this.domainEventGateway = domainEventGateway;
+        this.activationTokenGateway = activationTokenGateway;
     }
 
     public RegisterBuyerOutput execute(RegisterBuyerInput input) {
         Optional<User> optUser = userGateway.findByEmail(input.email());
         if (optUser.isPresent()) throw new EmailAlreadyInUseException("REGISTER_BUYER-001");
         String rawPassword = validatePassword(input.password(), "User", "REGISTER_BUYER-002");
-        return new RegisterBuyerOutput("Buyer registered successfully");
+        String encodedPassword = encoderGateway.encode(rawPassword);
+        User buyer = Buyer.register(input.firstName(),input.lastName(),input.email(),input.phoneNumber(),encodedPassword,input.address());
+        userGateway.save(buyer);
+        ActivationToken token = ActivationToken.generate(buyer.getId());
+        activationTokenGateway.save(token);
+        UserRegistered event = UserRegistered.of(buyer.getId(), buyer.getEmail(), buyer.getFirstName(), token.getId());
+        domainEventGateway.publish(event);
+        return new RegisterBuyerOutput("We've sent an activation link to provided email: " + input.email());
     }
 }
