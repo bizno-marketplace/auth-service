348556e442e19b506d205e4fdbd3ebc21c8ec5f2
gervasiodombo <gervasiorthur@gmil.com>
2026-05-18 08:56:47 +0200

    feat: ensure RegisterBuyer throws EmailAlreadyInUseException when email is already in use

--- src/main/java/com/biznopay/authservice/usecase/user/register/buyer/RegisterBuyer.java
+++ src/main/java/com/biznopay/authservice/usecase/user/register/buyer/RegisterBuyer.java
@@ -1,7 +1,23 @@
 package com.biznopay.authservice.usecase.user.register.buyer;
 
+import com.biznopay.authservice.domain.entity.user.User;
+import com.biznopay.authservice.domain.exception.EmailAlreadyInUseException;
+import com.biznopay.authservice.domain.gateway.UserGateway;
+
+import java.util.Optional;
+
 public class RegisterBuyer {
+    private final UserGateway userGateway;
+
+    public RegisterBuyer(UserGateway userGateway) {
+        this.userGateway = userGateway;
+    }
+
     public RegisterBuyerOutput execute(RegisterBuyerInput input) {
+        Optional<User> optUser = userGateway.findByEmail(input.email());
+        if (optUser.isPresent()) {
+            throw new EmailAlreadyInUseException("REGISTER_BUYER-001");
+        }
         return new RegisterBuyerOutput("Buyer registered successfully");
     }
 }
