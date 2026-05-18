2f7c954178eb63b6c4272feeb37af8c2a8ea2356
gervasiodombo <gervasiorthur@gmil.com>
2026-05-18 09:03:38 +0200

    feat: ensure RegisterBuyer throws RequiredFieldException when password is empty or null

--- src/main/java/com/biznopay/authservice/usecase/user/register/buyer/RegisterBuyer.java
+++ src/main/java/com/biznopay/authservice/usecase/user/register/buyer/RegisterBuyer.java
@@ -2,6 +2,8 @@ package com.biznopay.authservice.usecase.user.register.buyer;
 
 import com.biznopay.authservice.domain.entity.user.User;
 import com.biznopay.authservice.domain.exception.EmailAlreadyInUseException;
+import com.biznopay.authservice.domain.exception.InvalidPasswordException;
+import com.biznopay.authservice.domain.exception.RequiredFieldException;
 import com.biznopay.authservice.domain.gateway.UserGateway;
 
 import java.util.Optional;
@@ -18,6 +20,13 @@ public class RegisterBuyer {
         if (optUser.isPresent()) {
             throw new EmailAlreadyInUseException("REGISTER_BUYER-001");
         }
+        String rawPassword = validatePassword(input.password());
         return new RegisterBuyerOutput("Buyer registered successfully");
+    }
+
+    private String validatePassword(String password) {
+        if (password == null || password.isEmpty())
+            throw new RequiredFieldException("Password", User.class.getName(), "REGISTER_SA-003");
+        return password;
     }
 }
