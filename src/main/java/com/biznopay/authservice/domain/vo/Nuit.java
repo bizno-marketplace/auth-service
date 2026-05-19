7854fb96ac84ac41b9c6157d2d2cc9ec51b7bfce
gervasiodombo <gervasiorthur@gmil.com>
2026-05-19 21:31:34 +0200

    chore: improve code quality

--- src/main/java/com/biznopay/authservice/domain/vo/Nuit.java
+++ src/main/java/com/biznopay/authservice/domain/vo/Nuit.java
@@ -1,20 +1,18 @@
 package com.biznopay.authservice.domain.vo;
 
-import com.biznopay.authservice.domain.exception.InvalidFieldException;
 import com.biznopay.authservice.domain.exception.InvalidNuitException;
-import com.biznopay.authservice.domain.exception.InvalidStringFieldLengException;
 import com.biznopay.authservice.domain.exception.RequiredFieldException;
 
 public record Nuit(String value) {
     public Nuit {
         if (value == null || value.isBlank()) {
-            throw new RequiredFieldException("NUIT",Nuit.class.getName(),"NUIT-001");
+            throw new RequiredFieldException("NUIT", Nuit.class.getName(), "NUIT-001");
         }
         if (!value.matches("^\\d+$")) {
-            throw new InvalidNuitException(Nuit.class.getName(),"NUIT-002");
+            throw new InvalidNuitException(Nuit.class.getName(), "NUIT-002");
         }
         if (!value.matches("^\\d{9}$")) {
-            throw new InvalidNuitException(Nuit.class.getName(),"NUIT-003");
+            throw new InvalidNuitException(Nuit.class.getName(), "NUIT-003");
         }
     }
 }
