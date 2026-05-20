52b5a73f3d7ef701b9e1353c9a7e30c27ed86e0e
gervasiodombo <gervasiorthur@gmil.com>
2026-05-20 15:17:58 +0200

    chore: putting tests in pattens defined

---
+++ src/test/java/com/biznopay/authservice/testcases/ActivationTokenTestCases.java
@@ -0,0 +1,18 @@
+package com.biznopay.authservice.testcases;
+
+import com.biznopay.authservice.domain.entity.user.UserId;
+import com.biznopay.authservice.domain.exception.RequiredFieldException;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.UUID;
+import java.util.stream.Stream;
+
+public class ActivationTokenTestCases {
+    public static final UserId VALID_USER_ID = UserId.of(UUID.randomUUID());
+
+    public static Stream<Arguments> invalidDomainGenerateCases(){
+        return Stream.of(
+                Arguments.of("User id is null",null, RequiredFieldException.class, "UserId is required")
+        );
+    }
+}
