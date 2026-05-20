a421a1597a1f14f8b96fdc776f34bafe67260ef9
gervasiodombo <gervasiorthur@gmil.com>
2026-05-20 14:16:24 +0200

    feat: ensure RegisterSeller throws InvalidPasswordException when password is weak

--- src/main/java/com/biznopay/authservice/usecase/user/register/seller/RegisterSeller.java
+++ src/main/java/com/biznopay/authservice/usecase/user/register/seller/RegisterSeller.java
@@ -4,9 +4,10 @@ import com.biznopay.authservice.domain.entity.user.User;
 import com.biznopay.authservice.domain.exception.EmailAlreadyInUseException;
 import com.biznopay.authservice.domain.exception.NuitAlreadyInUseException;
 import com.biznopay.authservice.domain.gateway.UserGateway;
-import com.biznopay.authservice.domain.vo.Nuit;
 
 import java.util.Optional;
+
+import static com.biznopay.authservice.domain.util.DomainFuncUtils.validatePassword;
 
 public class RegisterSeller {
 
@@ -17,12 +18,17 @@ public class RegisterSeller {
     }
 
     public RegisterSellerOutput execute(RegisterSellerInput input) {
+        validateSeller(input);
+        String rawPassword = validatePassword(input.password(), "Seller", "REGISTER_SELLER-003");
+        return null;
+    }
+
+    private void validateSeller(RegisterSellerInput input){
         Optional<User> optUser = userGateway.findByEmail(input.email());
         if (optUser.isPresent())
             throw new EmailAlreadyInUseException("REGISTER_SELLER-001");
-        optUser =  userGateway.findByNuit(input.nuit());
+        optUser = userGateway.findByNuit(input.nuit());
         if (optUser.isPresent())
             throw new NuitAlreadyInUseException("REGISTER_SELLER-002");
-        return null;
     }
 }
